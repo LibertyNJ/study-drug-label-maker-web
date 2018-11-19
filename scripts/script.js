de0e5d07b9786f9c4955641d67021a91c314aee3
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2018-11-19 14:41:20 -0500

    Created standard and infusion label templates, previews, and sample placeholder text.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const printButton = document.getElementById('print-button');
+
+printButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  window.print();
+})
+
+window.onbeforeprint = prepareLabel;
+window.onafterprint = purgeLabel;
+
+function prepareLabel() {
+
+}
+
+function purgeLabel() {
+
+}
